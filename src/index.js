b17d4f7631bdebb8b034a5343328fa24dcb4effc
[raviraj0922] <[raviraj.0897@gmail.com]>
2024-08-09 12:39:30 +0530

    dtabase configuration

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+/*
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express"
+const app = express()
+
+( async() => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR:", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR:", error)
+        throw err
+    }
+})()
+
+*/
