51ba73de9ea849f1ada71a67a43bcc378466b287
[raviraj0922] <[raviraj.0897@gmail.com]>
2024-08-14 22:47:59 +0530

    configuration file uploaded

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch( (error)=>{
+    console.log("MONGO db connection failed !!!", error)
+})
 
 
 /*
