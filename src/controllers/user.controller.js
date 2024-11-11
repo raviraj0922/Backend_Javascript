07a52c515710b04739c173dbde31a39158ef0059
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2024-11-11 15:48:34 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,29 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { Router } from 'express'; // Add this import
+import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js"; // Corrected function name
+import {upload} from "../middlware/multer.middleware.js"
+import { verifyJWT } from '../middlware/auth.middleware.js';
 
+const router = Router();
 
-const resgisterUser = asyncHandler( async (req, res) => {
-    return res.status(200).json({
-        message: "Ok"
-    })
-})
+// POST route for user registration
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
 
+router.route("/login").post(loginUser)
 
-export {resgisterUser}
+// secure routes
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
+export default router;
